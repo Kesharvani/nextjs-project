236976606088458de0a56d6acc53b31cbb7e1acf
Kesharvani <shubhamkesharvani888@gmail.com>
2023-07-11 22:36:27 +0530

    feat:added login ui

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,48 @@
-"USE CLIENT"
+"use client"
+
+import Link from "next/link"
+
 export default function LoginPage(){
+    const onLogin=()=>{
+        console.log("Loggedin Successfully!!")
+    }
     return (
-        <div>This is login page</div>
+        <div className="h-[100vh] flex items-center justify-center">
+        <form
+          onSubmit={onLogin}
+          className="flex flex-col gap-[1rem] shadow-inner shadow-white p-[4rem]"
+        >
+          <h2 className="text-xl text-center font-bold">
+            Login to use an account
+          </h2>
+          <h4 className="mb-[1.5rem] text-center">
+            Don't have an account?{" "}
+            <Link href="/signup" className="underline">
+              Signup
+            </Link>
+          </h4>
+          <div className="flex flex-col gap-[0.2rem]">
+            <label htmlFor="email" className="flex-auto">
+              Email
+            </label>
+            <input type="text" name="email" className="p-[0.3rem] rounded-md" />
+          </div>
+  
+          <div className="flex flex-col gap-[0.2rem]">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              className="p-[0.3rem] rounded-md"
+            />
+          </div>
+          <button
+            type="submit"
+            className="border border-white rounded-md p-[0.5rem] mt-[1.5rem] bg-[#a82723] hover:bg-[#b92b27] cursor-pointer"
+          >
+            Login
+          </button>
+        </form>
+      </div>
     )
 }
