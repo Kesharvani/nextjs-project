a077415a97b8e27154ba373118196bfd7ff2f365
Kesharvani <shubhamkesharvani888@gmail.com>
2023-07-17 08:25:48 +0530

    feat:added logout functionality

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,5 +1,17 @@
+"use client"
+
+import axios from "axios"
+import { useRouter } from "next/navigation"
 export default function ProfilePage(){
+    const router=useRouter()
+    const logout=async()=>{
+        await axios.get("/api/users/logout")
+        router.push("/login")
+    }
     return (
+        <>
         <div>this is profile page</div>
+        <button onClick={logout}>Logout</button>
+        </>
     )
 }
