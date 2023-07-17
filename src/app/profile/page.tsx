1f1f96d6aef5e215721fbe34c4f10b84c0ed3320
Kesharvani <shubhamkesharvani888@gmail.com>
2023-07-17 10:54:28 +0530

    feat:added me route and helper funciton to fetch the jwt token data

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,17 +1,33 @@
-"use client"
+"use client";
 
-import axios from "axios"
-import { useRouter } from "next/navigation"
-export default function ProfilePage(){
-    const router=useRouter()
-    const logout=async()=>{
-        await axios.get("/api/users/logout")
-        router.push("/login")
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import Link from "next/link";
+export default function ProfilePage() {
+  const router = useRouter();
+  const [userId, setUserId] = useState("Nothing");
+  const logout = async () => {
+    await axios.get("/api/users/logout");
+    router.push("/login");
+  };
+
+  const getUserDetails = async () => {
+    try {
+      const {
+        data: { user },
+      } = await axios.get("/api/users/me");
+      setUserId(user._id);
+    } catch (error) {
+      console.error(error);
     }
-    return (
-        <>
-        <div>this is profile page</div>
-        <button onClick={logout}>Logout</button>
-        </>
-    )
+  };
+  return (
+    <>
+      <div>this is profile page</div>
+      <button onClick={logout}>Logout</button>
+      {userId === "Nothing" ? "Nothing" : <Link href={`/profile/${userId}`}>{userId}</Link>}
+      <button onClick={getUserDetails}>User Details</button>
+    </>
+  );
 }
